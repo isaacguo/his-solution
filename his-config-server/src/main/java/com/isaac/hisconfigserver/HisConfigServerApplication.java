863330445694be7918cc0b726f6b3912515d317d
GUO,YUE (K-China,ex1) <yue_guo@keysight.com>
2017-12-08 16:09:15 +0800

    initial version

---
+++ his-config-server/src/main/java/com/isaac/hisconfigserver/HisConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.isaac.hisconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HisConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HisConfigServerApplication.class, args);
+	}
+}
---
+++ his-eureka/src/main/java/com/isaac/hiseureka/HisEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.isaac.hiseureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HisEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HisEurekaApplication.class, args);
+	}
+}
