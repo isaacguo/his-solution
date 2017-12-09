85cdc8bacfb802ef72fee28ba5c386f942e63b9d
Isaac Guo <isaac_guo@163.com>
2017-12-09 22:54:56 +0800

    added first level components

--- his-ui/src/app.routing.ts
+++ his-ui/src/app.routing.ts
@@ -2,12 +2,84 @@ import {RouterModule, Routes} from "@angular/router";
 import {ModuleWithProviders} from "@angular/core";
 import {IndexComponent} from "./app/components/index/index.component";
 import {LoginComponent} from "./app/components/login/login.component";
+import {FrontDeskComponent} from "./app/components/front-desk/front-desk.component";
+import {TreatmentComponent} from "./app/components/treatment/treatment.component";
+import {TestingComponent} from "./app/components/testing/testing.component";
+import {ImagesComponent} from "./app/components/images/images.component";
+import {PharmacyComponent} from "./app/components/pharmacy/pharmacy.component";
+import {ProfilesComponent} from "./app/components/profiles/profiles.component";
+import {MembersComponent} from "./app/components/members/members.component";
+import {PetsComponent} from "./app/components/pets/pets.component";
+import {BusinessComponent} from "./app/components/business/business.component";
+import {ProcurementComponent} from "./app/components/procurement/procurement.component";
+import {InventoryComponent} from "./app/components/inventory/inventory.component";
+import {AnalysisComponent} from "./app/components/analysis/analysis.component";
+import {DataComponent} from "./app/components/data/data.component";
+import {SettingsComponent} from "./app/components/settings/settings.component";
 
 
 const appRoutes: Routes = [
   {
     path: '',
     component: IndexComponent,
+    children: [
+      {
+        path: 'frontdesk',
+        component: FrontDeskComponent
+      },
+      {
+        path: 'treatment',
+        component: TreatmentComponent
+      },
+      {
+        path: 'testing',
+        component:TestingComponent
+      },
+      {
+        path: 'images',
+        component: ImagesComponent
+      },
+      {
+        path: 'pharmacy',
+        component: PharmacyComponent
+      },
+      {
+        path: 'profiles',
+        component: ProfilesComponent
+      },
+      {
+        path: 'members',
+        component: MembersComponent
+      },
+      {
+        path: 'pets',
+        component: PetsComponent
+      },
+      {
+        path: 'business',
+        component: BusinessComponent
+      },
+      {
+        path: 'procurement',
+        component: ProcurementComponent
+      },
+      {
+        path: 'inventory',
+        component: InventoryComponent
+      },
+      {
+        path: 'analysis',
+        component: AnalysisComponent
+      },
+      {
+        path: 'data',
+        component: DataComponent
+      },
+      {
+        path: 'settings',
+        component: SettingsComponent
+      }
+    ]
   },
   {
     path: 'login',
--- his-ui/src/app/app.module.ts
+++ his-ui/src/app/app.module.ts
@@ -8,12 +8,42 @@ import {routing} from "../app.routing";
 import {HttpModule} from "@angular/http";
 import {FormsModule} from "@angular/forms";
 import {HashLocationStrategy, LocationStrategy} from "@angular/common";
+import {FrontDeskComponent} from './components/front-desk/front-desk.component';
+import {TreatmentComponent} from './components/treatment/treatment.component';
+import {ProfilesComponent} from './components/profiles/profiles.component';
+import {PetsComponent} from './components/pets/pets.component';
+import {BusinessComponent} from './components/business/business.component';
+import {InventoryComponent} from './components/inventory/inventory.component';
+import {AnalysisComponent} from './components/analysis/analysis.component';
+import {DataComponent} from './components/data/data.component';
+import {SystemComponent} from './components/system/system.component';
+import {SettingsComponent} from './components/settings/settings.component';
+import {TestingComponent} from './components/testing/testing.component';
+import {PharmacyComponent} from './components/pharmacy/pharmacy.component';
+import {ProcurementComponent} from './components/procurement/procurement.component';
+import {ImagesComponent} from './components/images/images.component';
+import { MembersComponent } from './components/members/members.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IndexComponent,
-    LoginComponent
+    LoginComponent,
+    FrontDeskComponent,
+    TreatmentComponent,
+    ProfilesComponent,
+    PetsComponent,
+    BusinessComponent,
+    InventoryComponent,
+    AnalysisComponent,
+    DataComponent,
+    SystemComponent,
+    SettingsComponent,
+    TestingComponent,
+    PharmacyComponent,
+    ProcurementComponent,
+    ImagesComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule, routing, HttpModule, FormsModule
