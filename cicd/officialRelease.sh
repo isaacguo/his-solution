8fee288e888686a9ae3556e1e9ebc60ff53deb4a
Isaac Guo <isaac_guo@163.com>
2017-12-31 00:25:52 +0800

    added officialRelease.sh script

---
+++ cicd/officialRelease.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+branch_name="$(git symbolic-ref HEAD 2>/dev/null)" || branch_name="(unnamed branch)"     # detached HEAD
+branch_name=${branch_name##refs/heads/}
+
+if [[ $branch_name == release/* ]]; then
+  echo 'this is a release branch'
+else
+  echo 'this is not a release barnch, please swith to a release branch'
+fi
+
+
+versionNumber=`echo $branch_name | awk -F '[/ -]' '{ print $2 }'`
+echo $versionNumber
+
+
+git checkout master
+git merge --no-ff $branch_name
+
+cd `pwd`/..
+mvn versions:set -DnewVersion=$versionNumber
+mvn versions:commit
+
+git commit -am 'update to release version:' v$versionNumber
+
+git tag -a v$versionNumber
+
