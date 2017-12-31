1ff2f4adb54dc9de5163d66a457fd9b436ae7623
Isaac Guo <isaac_guo@163.com>
2017-12-31 13:19:02 +0800

    added gateway service

---
+++ his-gateway/src/main/java/com/isaac/hisgateway/HisGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.isaac.hisgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HisGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HisGatewayApplication.class, args);
+	}
+}
