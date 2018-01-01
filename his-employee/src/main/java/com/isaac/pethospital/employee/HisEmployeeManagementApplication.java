211823f8a3ffc2b1e77faec8bcb6cc7b4e888196
Isaac Guo <isaac_guo@163.com>
2018-01-01 23:55:17 +0800

    enable eureka client, configure config repo

--- his-employee/src/main/java/com/isaac/pethospital/employee/HisEmployeeManagementApplication.java
+++ his-employee/src/main/java/com/isaac/pethospital/employee/HisEmployeeManagementApplication.java
@@ -2,8 +2,10 @@ package com.isaac.pethospital.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class HisEmployeeManagementApplication {
 
 	public static void main(String[] args) {
