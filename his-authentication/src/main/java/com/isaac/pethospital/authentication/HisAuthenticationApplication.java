94d22cfcf5155a3116eb8bd6576d45deadf930b3
Isaac Guo <isaac_guo@163.com>
2018-01-07 00:26:40 +0800

    added an empty authentication service

---
+++ his-authentication/src/main/java/com/isaac/pethospital/authentication/HisAuthenticationApplication.java
@@ -0,0 +1,12 @@
+package com.isaac.pethospital.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HisAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HisAuthenticationApplication.class, args);
+	}
+}
---
+++ his-authentication/src/test/java/com/isaac/pethospital/authentication/HisAuthenticationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.isaac.pethospital.authentication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HisAuthenticationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
