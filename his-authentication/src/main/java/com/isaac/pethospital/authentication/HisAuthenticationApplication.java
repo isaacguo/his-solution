edd20b1d5fd76aa348993f3b62bfcb78b617c703
Isaac Guo <isaac_guo@163.com>
2018-01-07 02:10:12 +0800

    remove dependencymanagement section in modules

--- his-authentication/src/main/java/com/isaac/pethospital/authentication/HisAuthenticationApplication.java
+++ his-authentication/src/main/java/com/isaac/pethospital/authentication/HisAuthenticationApplication.java
@@ -2,6 +2,7 @@ package com.isaac.pethospital.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
