1eba88cc0d61a6933f06690600edaab1bd900f4c
Isaac Guo <isaac_guo@163.com>
2018-01-07 02:00:40 +0800

    added support to authentication for eureka discovery and config client

--- his-authentication/src/main/java/com/isaac/pethospital/authentication/HisAuthenticationApplication.java
+++ his-authentication/src/main/java/com/isaac/pethospital/authentication/HisAuthenticationApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class HisAuthenticationApplication {
 
 	public static void main(String[] args) {
