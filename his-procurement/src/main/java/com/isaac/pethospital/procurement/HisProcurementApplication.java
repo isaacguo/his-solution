6544d9ab6e6124a9fdf24c7effd3cc69b1cfddee
Isaac Guo <isaac_guo@163.com>
2018-03-27 06:07:08 +0800

    added procurement service

---
+++ his-procurement/src/main/java/com/isaac/pethospital/procurement/HisProcurementApplication.java
@@ -0,0 +1,22 @@
+package com.isaac.pethospital.procurement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+@ComponentScan(basePackages = {"com.isaac.pethospital"})
+@EntityScan(basePackages = {"com.isaac.pethospital"})
+@EnableJpaRepositories(basePackages = {"com.isaac.pethospital"})
+public class HisProcurementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HisProcurementApplication.class, args);
+    }
+
+}
