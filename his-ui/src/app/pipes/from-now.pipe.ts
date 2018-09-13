25608776fca0cecf5710b0d103bea51ee829ebae
Isaac Guo <isaac_guo@163.com>
2018-09-13 15:51:03 +0800

    changes

--- his-ui/src/app/pipes/from-now.pipe.ts
+++ his-ui/src/app/pipes/from-now.pipe.ts
@@ -1,13 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import * as moment from 'moment';
 
+moment.locale('zh-cn');
+
 @Pipe({
   name: 'fromNow'
 })
 export class FromNowPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    moment.locale("zh-cn")
     return moment(value).fromNow();
   }
 
