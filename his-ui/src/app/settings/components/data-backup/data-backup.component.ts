cab25cd56d2864ae706b6d7b03787d56adc794f2
Isaac Guo <isaac_guo@163.com>
2018-11-04 14:13:47 +0800

    changes

--- his-ui/src/app/settings/components/data-backup/data-backup.component.ts
+++ his-ui/src/app/settings/components/data-backup/data-backup.component.ts
@@ -1,4 +1,16 @@
-import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ViewChild
+} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ActivatedRoute} from "@angular/router";
+import {ModalComponent} from "ng2-bs3-modal/ng2-bs3-modal";
 
 @Component({
   selector: 'app-data-backup',
@@ -7,25 +19,56 @@ import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 
 })
-export class DataBackupComponent implements OnInit {
+export class DataBackupComponent implements OnInit, OnChanges {
 
   @Input()
   pageData: any;
+  @Output()
+  scheduleInfo: any;
+
+  @ViewChild("scheduleBackupModal")
+  scheduleBackupModal: ModalComponent;
+
+  formModel: FormGroup;
 
   selectedStatusText: string = '1';
 
   getHoursArr() {
     return Array.from(Array(23).keys()).map(i => i + 1);
   }
-  constructor() {
+
+  constructor(public route: ActivatedRoute,
+              private fb: FormBuilder) {
+
+    this.formModel = this.fb.group({
+      'scheduleType': ['', Validators.required],
+      'hourValue': ['1', Validators.required]
+    })
   }
+
 
   ngOnInit() {
   }
 
 
-  onPageChanged($event)
-  {
+  onPageChanged($event) {
 
+  }
+
+  onHourClicked(hour: string) {
+    this.formModel.controls['hourValue'].setValue(hour);
+  }
+
+  onScheduleBackupClicked() {
+    this.scheduleBackupModal.open();
+  }
+
+  onScheduleBackupClosed() {
+
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['scheduleInfo'] && this.scheduleInfo)
+      this.formModel.patchValue(this.scheduleInfo);
   }
 }
