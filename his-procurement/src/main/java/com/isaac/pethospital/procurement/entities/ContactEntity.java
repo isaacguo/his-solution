c09d8f57857cd423f941763389ec314c69a01b0a
Isaac Guo <isaac_guo@163.com>
2018-11-25 15:25:13 +0800

    update schema

--- his-procurement/src/main/java/com/isaac/pethospital/procurement/entities/ContactEntity.java
+++ his-procurement/src/main/java/com/isaac/pethospital/procurement/entities/ContactEntity.java
@@ -3,7 +3,6 @@ package com.isaac.pethospital.procurement.entities;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import javax.persistence.*;
-import java.util.List;
 
 @Entity
 public class ContactEntity {
