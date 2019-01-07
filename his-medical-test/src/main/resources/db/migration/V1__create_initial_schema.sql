53f9c289a5466e608c558ca81a06ad6a75d69c44
Isaac Guo <isaac_guo@163.com>
2019-01-07 09:34:49 +0800

    update db schema

--- his-medical-test/src/main/resources/db/migration/V1__create_initial_schema.sql
+++ his-medical-test/src/main/resources/db/migration/V1__create_initial_schema.sql
@@ -7,9 +7,8 @@ create table department_entity (id bigint not null auto_increment, dep_id bigint
 create table report_entity (id bigint not null auto_increment, created_date_time datetime, finished_date_time datetime, paid_date_time datetime, pet_owner_uuid varchar(255), pet_uuid varchar(255), report_name varchar(255), report_status integer, report_template_uuid varchar(255), treatment_case_uuid varchar(255), uuid varchar(255), primary key (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 create table report_info_entity (id bigint not null auto_increment, report_key varchar(255), report_section varchar(255), report_value varchar(255), report_id bigint, primary key (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 create table report_item_entity (id bigint not null auto_increment, comments varchar(255), item_name varchar(255), item_unit varchar(255), reference_high_limit_value varchar(255), reference_low_limit_value varchar(255), result varchar(255), report_id bigint, primary key (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-create table report_template_category_entity (id bigint not null auto_increment, name varchar(255), parent_id bigint, primary key (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-create table report_template_entity (id bigint not null auto_increment, report_name varchar(255), report_name_han_yu_pin_yin varchar(255), uuid varchar(255), category_id bigint, primary key (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-create table report_template_entity_department_list (supported_report_templates_id bigint not null, department_list_id bigint not null) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table report_template_category_entity (id bigint not null auto_increment, name varchar(255), department_id bigint, parent_id bigint, primary key (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table report_template_entity (id bigint not null auto_increment, report_name varchar(255), report_name_han_yu_pin_yin varchar(255), uuid varchar(255), category_id bigint, department_id bigint, primary key (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 create table report_template_info_entity (id bigint not null auto_increment, report_key varchar(255), report_section varchar(255), report_value varchar(255), report_template_id bigint, primary key (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 create table report_template_item_entity (id bigint not null auto_increment, comments varchar(255), item_name varchar(255), item_unit varchar(255), reference_high_limit_value varchar(255), reference_low_limit_value varchar(255), report_template_id bigint, primary key (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 create table topic_operation_entity (id bigint not null auto_increment, name varchar(255), authorization_topic_id bigint, primary key (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -20,10 +19,10 @@ alter table authorization_assignment_entity_allowed_operations add constraint FK
 alter table authorization_assignment_entity_allowed_operations add constraint FKngkgigjkrhs9p2ir6bh9cuu6a foreign key (authorization_assignment_entity_id) references authorization_assignment_entity (id);
 alter table report_info_entity add constraint FKapcitetv62qlykq00wmphc1o0 foreign key (report_id) references report_entity (id);
 alter table report_item_entity add constraint FKp925curaqjcvb8hal0m9ick42 foreign key (report_id) references report_entity (id);
+alter table report_template_category_entity add constraint FKrb14k66i30myicymaghfpxplo foreign key (department_id) references department_entity (id);
 alter table report_template_category_entity add constraint FK9cjmxo9biqu4e2duj95i88lbj foreign key (parent_id) references report_template_category_entity (id);
 alter table report_template_entity add constraint FK4n1wf35cfew97in891l1ktr7y foreign key (category_id) references report_template_category_entity (id);
-alter table report_template_entity_department_list add constraint FKlubb5803rimg601mt4amd67o8 foreign key (department_list_id) references department_entity (id);
-alter table report_template_entity_department_list add constraint FK8st1e38c1dqcubddyac789wx4 foreign key (supported_report_templates_id) references report_template_entity (id);
+alter table report_template_entity add constraint FKo99ncaeexhvnup2n4h5ctmfx0 foreign key (department_id) references department_entity (id);
 alter table report_template_info_entity add constraint FK894ypqlpfiight4vw4kiqn0bm foreign key (report_template_id) references report_template_entity (id);
 alter table report_template_item_entity add constraint FKh2lko2hwf7aeepnpsl30x71uf foreign key (report_template_id) references report_template_entity (id);
 alter table topic_operation_entity add constraint FKoec3nr2ausdy7u53qubpxopo8 foreign key (authorization_topic_id) references authorization_topic_entity (id);
